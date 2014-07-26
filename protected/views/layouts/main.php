b5623f52fa63dfa87a892b196ac075980e9e38ff
Harutyun Margaryan <harutyun_margaryan@edu.aua.am>
2014-07-27 00:51:20 +0400

    save6- tree

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,13 +5,6 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
 
-	<!--<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php /*echo Yii::app()->request->baseUrl; */?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php /*echo Yii::app()->request->baseUrl; */?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php /*echo Yii::app()->request->baseUrl; */?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
---
+++ protected/views/site/tree.php
@@ -0,0 +1,52 @@
+<?php
+    /* @var  $users  User[] */
+
+    Yii::app()->getclientscript()->registerCssFile(Yii::app()->baseUrl."/css/tree.css");
+
+function drawTree($users) {
+    echo "<ul>";
+    foreach($users->users as $user) {
+        echo "<li><span><i class='glyphicon-minus'></i> </span>$user->username";
+        if(!empty($user->users))
+            drawTree($user);
+        echo "</li>";
+    }
+    echo "</ul>";
+}
+
+?>
+
+<div class="panel panel-default">
+    <div class="panel-heading-small2"> <a data-toggle="collapse" data-parent="#accordion" href="#collapseDVR3">
+            <h4 class="panel-title">
+                Users
+            </h4>
+        </a>
+
+    </div>
+    <div id="collapseDVR3" class="panel-collapse collapse in">
+        <div class="tree ">
+
+           <?php drawTree($users); ?>
+
+        </div>
+    </div>
+</div>
+
+
+<script>
+    $(function () {
+        $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+        $('.tree li.parent_li > span').on('click', function (e) {
+            var children = $(this).parent('li.parent_li').find(' > ul > li');
+            if (children.is(":visible")) {
+                children.hide('fast');
+                $(this).attr('title', 'Expand this branch').find(' > i').addClass('glyphicon-plus').removeClass('glyphicon-minus');
+            } else {
+                children.show('fast');
+                $(this).attr('title', 'Collapse this branch').find(' > i').addClass('glyphicon-minus').removeClass('glyphicon-plus');
+            }
+            e.stopPropagation();
+        });
+    });
+</script>
